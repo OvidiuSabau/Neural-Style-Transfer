44c557636466d43f5cd6bf1f7f6851129cff1b40
Ovidiu Sabau <sabau.ovidiu12@gmail.com>
2018-09-01 13:53:03 +0300

    Added comments

--- backend.py
+++ backend.py
@@ -3,15 +3,19 @@ import gc
 from utils import *
 from vgg19 import Vgg19
 
+
 def model(content_path, style_path, alpha, beta, lambdas, num_iterations, initial_learning_rate, learning_rate_decay):
+
+    # Code to make Tensorflow release GPU Memory after it's done
     tf.reset_default_graph()
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
 
-    # Read the content and style images and create a noisy version of the content image to be the target_image
+    # Read the content and style images, normalize them and also save the normalizing values
     content_means, content_variances, content_image = readAndNormalize(content_path)
     style_means, style_variances, style_image = readAndNormalize(style_path)
 
+    # Compute the weighted average of the means and variances
     means = (alpha * content_means + beta * style_means) / (alpha + beta)
     variances = (alpha * content_variances + beta * style_variances) / (alpha + beta)
 
@@ -20,43 +24,62 @@ def model(content_path, style_path, alpha, beta, lambdas, num_iterations, initia
     style_image = style_image[np.newaxis, ...].astype(dtype=np.float32)
     target_means, target_variances, target_image = addGausianNoise(content_image[0, :, :, :])
 
+    # Create the 3 model instances we need
     content_model = Vgg19()
     style_model = Vgg19()
     target_model = Vgg19()
 
+    # Create the dictionaries we'll use to store the values computed
     style_activations = dict()
     target_style_activations = dict()
-    layer_costs = dict()
 
+    # Define the target image to be a Tensorflow variable and initiate it to the noisy image
     target_image_variable = tf.get_variable(name='target_activation', dtype=tf.float32, initializer=target_image)
+
+    # Build the instance for the content and calculate the activation for a layer
     content_model.build(content_image)
     content_activation = content_model.conv3_1
+
+    # Build the instance for the style and calculate the activations for the layers we want
     style_model.build(style_image)
     style_activations['conv2_1'] = style_model.conv2_1
     style_activations['conv2_2'] = style_model.conv2_2
     style_activations['conv3_1'] = style_model.conv3_1
     style_activations['conv3_2'] = style_model.conv3_2
+
+    # Build the instance for the target image and calculate the activations for the layers we used for CONTENT and STYLE
+    # Make sure that you use the same layers as you did for the content and style activations
     target_model.build(target_image_variable)
     target_content_activation = target_model.conv3_1
     target_style_activations['conv2_1'] = target_model.conv2_1
     target_style_activations['conv2_2'] = target_model.conv2_2
     target_style_activations['conv3_1'] = target_model.conv3_1
     target_style_activations['conv3_2'] = target_model.conv3_2
+
+    # Calculate the costs using the functions defined in the utils file
     content_cost = contentCost(content_activation, target_content_activation)
     style_cost = styleCost(style_activations, target_style_activations, lambdas)
     total_cost = totalCost(content_cost, style_cost, alpha, beta)
 
+    # Define the optimizer
     learning_rate = tf.placeholder(name='learning_rate', dtype=tf.float32)
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
     train_step = optimizer.minimize(total_cost)
 
+    # Start the session using the config settings defined at the beginning of the file
     with tf.Session(config=config) as session:
 
+        # Initialize the variable
         session.run(tf.global_variables_initializer())
+
+        # Run the loop for the specified number of times
         for iteration in range(num_iterations):
 
+            # Run one iteration of the training stept
             session.run(train_step,
                         feed_dict={learning_rate: initial_learning_rate / (1 + learning_rate_decay * iteration)})
+
+            # Print the cost every X iterations
             if iteration % 10 == 0:
                 current_content_cost, current_style_cost = session.run([content_cost, style_cost])
                 print("Iteration number {}: {} ~ 10^{}  <--->  {} ~ 10^{} ".format(iteration, current_content_cost,
@@ -65,8 +88,13 @@ def model(content_path, style_path, alpha, beta, lambdas, num_iterations, initia
                                                                                    current_style_cost, np.floor(
                         np.log10(current_style_cost))))
 
+        # Save the final output
         output, final_style_cost, final_content_cost = session.run([target_image_variable, style_cost, content_cost])
+
+        # Revert the normalization
         output = output[0, :, :, :] * content_variances + content_means
+
+        # Save the image
         content_path = content_path[:-4]
         content_path = content_path[17:]
         style_path = style_path[:-4]
@@ -75,5 +103,7 @@ def model(content_path, style_path, alpha, beta, lambdas, num_iterations, initia
         cv2.imwrite(name, output)
         print(name + " was created. Final content cost was {}; Final style cost was {}.".format(final_content_cost,
                                                                                                 final_style_cost))
+
+        # Close the session and free the unused memory
         session.close()
         gc.collect()
--- frontend.py
+++ frontend.py
@@ -1,16 +1,23 @@
-from modelBackend import model
+from backend import model
 import os
-import cv2
 
+# content path --------> the path to the content image
+# style path ----------> the path to the style image
+# alpha ---------------> the higher the alpha relative to beta, the more the final image will resemble the content image
+# beta ----------------> the higher the beta relative to alpha, the more the final image will resemble the style image
+# lambdas -------------> a list of values specifying how much weight to attribute to each layer activation
+# num_iterations ------> how many training steps the program should run
+# learning_rate -------> how big the adjustments on every training step should be
+# learning rate_decay -> adjusts how much the learning rate decreases over the course of training
 
 content_path  = "./content images/giza.jpg"
-style_path = "./style images/the-great-wave.jpg"
+style_path = "./style images/erin_hanson.png"
 alpha = 7*1000000000
 beta = 1
 lambdas = [0.25, 0.25, 0.25, 0.25]
-num_iterations = 2000
-learning_rate = 0.22
-learning_rate_decay = 0.002
+num_iterations = 1500
+learning_rate = 0.27
+learning_rate_decay = 0.009
 
 # for content_path in os.listdir("./content images"):
 #     for style_path in os.listdir("./style images"):
